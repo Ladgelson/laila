9e2de07bde9115a45aeeb720efd4a681fff443e2
Ladgelson <micaiasladgelsondasilva@gmail.com>
2021-03-14 23:01:28 -0300

    test 2

--- backend/resources/lexical.py
+++ backend/resources/lexical.py
@@ -12,7 +12,7 @@ class Lexical(Resource):
             'hash': uniqueId
         }
 
-        resp = requests.get('http://0.0.0.0:80/lexical', json=obj)
+        resp = requests.post('http://0.0.0.0:80/lexical', json=obj)
         # resp = requests.post('http://200.129.3.5:80/lexical', json=obj)
         # if resp.status_code > 202:
         #     return {'message': 'Something went wrong!'}, 500
